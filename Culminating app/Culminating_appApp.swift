4b652e5964a0de86b9bdf0a47b774a7efa428acf
grayden clark <gclark@lcs.on.ca>
2024-05-31 15:01:03 -0400

    Initial Commit

---
+++ Culminating app/Culminating_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Culminating_appApp.swift
+//  Culminating app
+//
+//  Created by Gray Clark on 2024-05-31.
+//
+
+import SwiftUI
+
+@main
+struct Culminating_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
